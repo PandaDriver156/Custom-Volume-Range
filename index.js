daeb6ee73646a55f490058015df4dee9b3a5d522
PandaDriver <pandadriver156@gmail.com>
2022-08-15 16:18:56 +0200

    Use only one injection

--- index.js
+++ index.js
@@ -27,24 +27,21 @@ module.exports = class CustomVolumeRange extends Plugin {
         // pluginSettings must be a separate variable because 'this' refers to the module we inject into
         let pluginSettings = this.settings;
 
-        // User volume
-        inject('custom-volume-range_userslider', Slider.prototype, 'render', function (args) {
-            const maxVolume = pluginSettings.get('maxAdjustableVolume', defaultMaxVolume);
-            // only change range if label is 'User Volume'
-            if (this.props && this.props['aria-label'] === Messages.USER_VOLUME) {
-                this.props.maxValue = maxVolume;
-                this.state.value = this.state.initialValueProp;
-                this.state.max = maxVolume;
-                this.state.range = this.state.max;
-            }
-            return args;
-        }, true);
+        inject('custom-volume-range_slider', Slider.prototype, 'render', function (args) {
+            if (this.props) {
+                let maxVolume = 0;
+                // only change range if label is 'User Volume' or 'Stream Volume'
+                switch (this.props['aria-label']) {
+                    case Messages.USER_VOLUME:
+                        maxVolume = pluginSettings.get('maxAdjustableVolume', defaultMaxVolume);
+                        break;
+                    case Messages.STREAM_VOLUME:
+                        maxVolume = pluginSettings.get('maxAdjustableStreamVolume', defaultMaxStreamVolume);
+                        break;
+                    default:
+                        return args;
+                }
 
-        // Stream volume (slider on right-click)
-        inject('custom-volume-range_streamslider', Slider.prototype, 'render', function (args) {
-            const maxVolume = pluginSettings.get('maxAdjustableStreamVolume', defaultMaxStreamVolume);
-            // only change range if label is 'Stream Volume'
-            if (this.props && this.props['aria-label'] === Messages.STREAM_VOLUME) {
                 this.props.maxValue = maxVolume;
                 this.state.value = this.state.initialValueProp;
                 this.state.max = maxVolume;
@@ -55,8 +52,7 @@ module.exports = class CustomVolumeRange extends Plugin {
     }
 
     pluginWillUnload() {
-        uninject('custom-volume-range_userslider');
-        uninject('custom-volume-range_streamslider');
+        uninject('custom-volume-range_slider');
         powercord.api.settings.unregisterSettings('custom-volume-range_settings');
     }
 };
